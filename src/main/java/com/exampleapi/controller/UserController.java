c8cb405b8f4f37da37bd55c744732fce67979364
CBGoncalves <carlos.eduardo46851@gmail.com>
2024-03-18 15:56:50 -0300

    first commit / primeira api feita em spring boot

---
+++ src/main/java/com/exampleapi/controller/UserController.java
@@ -0,0 +1,25 @@
+package com.exampleapi.controller;
+
+import com.exampleapi.model.User;
+import com.exampleapi.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/exampleapi")
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping
+    public List<User> getUsers() {
+        return userRepository.findAll();
+    }
+
+}
